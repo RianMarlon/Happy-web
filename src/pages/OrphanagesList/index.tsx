1778b25390453eacc961abb0faf3432255102d19
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-16 11:20:08 -0300

    Adicionando o componente Marker na página OrphanagesList no projeto web

--- src/pages/OrphanagesList/index.tsx
+++ src/pages/OrphanagesList/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+
+import Leaflet from 'leaflet';
 
 import mapMarkerImg from '../../assets/images/map-marker.svg';
 
 import 'leaflet/dist/leaflet.css';
 import './styles.css';
+
+const mapIcon = Leaflet.icon({
+  iconUrl: mapMarkerImg,
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+});
 
 function OrphanagesList() {
   return (
@@ -25,12 +33,17 @@ function OrphanagesList() {
       </aside>
 
       <Map 
-        center={[-5.1074733, -38.3692486]}
+        center={[-5.1069647,-38.3761372]}
         zoom={15}
         style={{ flex: 1 }}
       >
         <TileLayer 
           url={`https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} 
+        />
+
+        <Marker 
+          icon={mapIcon}
+          position={[-5.1069647,-38.3761372]}
         />
       </Map>
 
