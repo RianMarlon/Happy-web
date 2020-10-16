b14af64921094e787e4867ea98e7aad7cad04219
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-16 13:17:30 -0300

    Adicionando o mapIcon nas páginas CreateOrphanage e Orphanage no projeto web

--- src/utils/mapIcon.ts
+++ src/utils/mapIcon.ts
@@ -1,14 +1,12 @@
 import L from 'leaflet';
 
-import mapMarkerImg from '../../assets/images/map-marker.svg';
+import mapMarkerImg from '../assets/images/map-marker.svg';
 
-import './styles.css';
-
-const happyMapIcon = L.icon({
+const mapIcon = L.icon({
   iconUrl: mapMarkerImg,
   iconSize: [58, 68],
   iconAnchor: [29, 68],
   popupAnchor: [0, -60],
 });
 
-export default happyMapIcon;
+export default mapIcon;
