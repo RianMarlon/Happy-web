7bbf6ccacf9b3b5e6ecae11b5e4995dd2e313195
Rian Marlon <rianmarlon20@yahoo.com>
2020-11-28 23:33:11 -0300

    Alterando a estrutura da página Landing do projeto web

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -3,6 +3,7 @@ import { FiArrowRight } from 'react-icons/fi'
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
+import landingImg from '../../assets/images/landing.svg';
 
 import './styles.css';
 
@@ -10,21 +11,29 @@ function Landing() {
   return (
     <div id="page-landing">
       <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
+        <header>
+          <img src={logoImg} alt="Happy" />
+
+          <div className="location">
+            <strong>Morada Nova</strong>
+            <span>Ceará</span>
+          </div>
+        </header>
+
+        <div className="image-landing-container">
+          <img src={landingImg} alt="" />
+        </div>
 
         <main>
           <h1>Leve felicidade para o mundo</h1>
           <p>Visite orfanatos e mude o dia de muitas crianças.</p>
         </main>
 
-        <div className="location">
-          <strong>Morada Nova</strong>
-          <span>Ceará</span>
+        <div className="enter-app-container">
+          <Link to="/app" className="enter-app">
+            <FiArrowRight size={24} color="rgba(0, 0, 0, 0.6)" />
+          </Link>
         </div>
-
-        <Link to="/app" className="enter-app">
-          <FiArrowRight size={24} color="rgba(0, 0, 0, 0.6)" />
-        </Link>
       </div>
     </div>
   )
