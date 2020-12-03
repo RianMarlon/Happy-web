9da25aa61558dcb01f7c0d882c45774164747046
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-03 11:34:08 -0300

    Adicionando a estrutura do componente Input do projeto web

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,43 @@
+import React, { InputHTMLAttributes, useState, useEffect } from 'react';
+
+import './styles.css';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+  labelError: string;
+  error: boolean;
+}
+
+function Input({ label, labelError, error, ...rest }: InputProps) {
+
+  const labelClassName = error ? 'error' : '';
+
+  const [inputClassName, setInputClassName] = useState('input');
+  
+  useEffect(() => {
+    if (rest.value && rest.value.toString().trim()) {
+      setInputClassName('input no-empty');
+    }
+
+    else if (error) {
+      setInputClassName('input input-error');
+    }
+
+    else {
+      setInputClassName('input');
+    }
+  }, [rest.value, error]);
+
+  return (
+    <div className="input-block">
+      <label className={labelClassName} htmlFor={rest.name}>
+        {
+          error ? labelError : label
+        }
+      </label>
+      <input className={inputClassName} { ...rest } />
+    </div>
+  );
+}
+
+export default Input;
