09d4aec6c85f754c1e07c92f80837a38b8b06eb0
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-10 12:05:02 -0300

    Alterando o mapIcon da pasta utils do projeto web

--- src/utils/mapIcon.ts
+++ src/utils/mapIcon.ts
@@ -4,9 +4,9 @@ import mapMarkerImg from '../assets/images/map-marker.svg';
 
 const mapIcon = L.icon({
   iconUrl: mapMarkerImg,
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60],
+  iconSize: [50, 60],
+  iconAnchor: [25, 60],
+  popupAnchor: [170, 8],
 });
 
 export default mapIcon;
