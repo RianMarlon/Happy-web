a862910bfb04ed84a7fdb0f6ed1d2b58e30720bf
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-26 22:19:34 -0300

    Adicionando a rota para a página de login no routes.tsx do projeto web

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+import Login from './pages/Login';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
 import Orphanage from './pages/Orphanage';
@@ -10,6 +11,8 @@ function Routes() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/login" component={Login} />
+
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
         <Route path="/orphanages/create" component={CreateOrphanage} />
