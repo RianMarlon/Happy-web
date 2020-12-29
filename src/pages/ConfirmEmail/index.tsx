3294a90d7e424cf6f940a72c130b770ef9dd6aa1
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-29 14:17:07 -0300

    Adicionando a estrutura da página ConfirmEmail do projeto web

---
+++ src/pages/ConfirmEmail/index.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import Logo from '../../components/Logo';
+import Success from '../../components/Success';
+
+import './styles.css';
+
+function ConfirmEmail(props: any) {
+
+  const history = useHistory();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const query = props.location.search;
+    const token = query.split('token=')[1];
+    
+    const data = {
+      token
+    }
+
+    api.put('/confirm-email', data)
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        history.push('/login');
+      });
+    // eslint-disable-next-line
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="loading-container">
+        <Logo />
+      </div>
+    );
+  }
+
+  return (
+    <Success 
+      title="E-mail confirmado!"
+      description="Seu e-mail foi confirmado, agora você pode realizar o login no Happy."
+      textButton="Voltar ao login"
+      routeButton="/login"
+    />
+  );
+}
+
+export default ConfirmEmail;
