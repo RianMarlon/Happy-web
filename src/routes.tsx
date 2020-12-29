0429b24eef2cdd863529e98157073a9825f4e725
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-29 14:18:32 -0300

    Adicionando a rota confirm-email no routes.tsx do projeto web

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
+import ConfirmEmail from './pages/ConfirmEmail';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
 import Orphanage from './pages/Orphanage';
@@ -16,11 +17,12 @@ function Routes() {
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
         <Route path="/forgot-password" component={ForgotPassword} />
+        <Route path="/confirm-email" component={ConfirmEmail} />
 
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
         <Route path="/orphanages/create" component={CreateOrphanage} />
-        <Route path="/orphanages/:id" component={Orphanage} />
+        <Route path="/orphanages/:id" exact component={Orphanage} />
       </Switch>
     </BrowserRouter>
   );
