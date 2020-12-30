33a38d9a48503085efa8c4eb26b2a7127526ee10
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-30 10:38:10 -0300

    Adicionando a rota change-password no routes.tsx do projeto web

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
+import ChangePassword from './pages/ChangePassword';
 import ConfirmEmail from './pages/ConfirmEmail';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
@@ -16,8 +17,9 @@ function Routes() {
       <Switch>
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
-        <Route path="/forgot-password" component={ForgotPassword} />
         <Route path="/confirm-email" component={ConfirmEmail} />
+        <Route path="/forgot-password" component={ForgotPassword} />
+        <Route path="/change-password" component={ChangePassword} />
 
         <Route path="/" exact component={Landing} />
         <Route path="/app" component={OrphanagesMap} />
