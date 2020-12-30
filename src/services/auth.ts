56483b8e4043a8514e7ba7aafc04f959f22ccd5a
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-30 10:33:29 -0300

    Adicionando o auth.ts na pasta services do projeto web

---
+++ src/services/auth.ts
@@ -0,0 +1,75 @@
+import api from './api';
+
+export const TOKEN_KEY = '@happy/user';
+
+export const hasToken = () => {
+  const tokenLocalStorage = localStorage.getItem(TOKEN_KEY);
+  const tokenSessionStorage = sessionStorage.getItem(TOKEN_KEY);
+
+  return !!tokenLocalStorage || !!tokenSessionStorage
+};
+
+export const hasTokenValid = async () => {
+  if(hasToken()) {
+    const tokenLocalStorage = localStorage.getItem(TOKEN_KEY);
+    const tokenSessionStorage = sessionStorage.getItem(TOKEN_KEY);
+
+    const hasTokenInLocalStorage = tokenLocalStorage !== null;
+    const hasTokenInSessionStorage = tokenSessionStorage !== null;
+
+    if (hasTokenInLocalStorage) {
+      const data = {
+        token: tokenLocalStorage
+      }
+
+      const response = await api.post('/validate-token', data);
+      const { is_token_valid: isTokenValid, is_admin: isAdmin } = response.data;
+
+      if (!isTokenValid) {
+        localStorage.removeItem(TOKEN_KEY);
+      }
+
+      return {
+        isTokenValid,
+        isAdmin
+      };
+    }
+
+    if (hasTokenInSessionStorage) {
+      const data = {
+        token: tokenSessionStorage
+      }
+
+      const response = await api.post('/validate-token', data);
+      const { is_valid_token: isTokenValid, id_admin: isAdmin } = response.data;
+      
+      if (!isTokenValid) {
+        sessionStorage.removeItem(TOKEN_KEY);
+      }
+
+      return {
+        isTokenValid,
+        isAdmin
+      };
+    }
+  }
+
+  else {
+    return {
+      isTokenValid: false,
+      isAdmin: false
+    };
+  }
+};
+
+export const getToken = () => {
+  const tokenLocalStorage = localStorage.getItem(TOKEN_KEY);
+  const tokenSessionStorage = sessionStorage.getItem(TOKEN_KEY);
+
+  return tokenLocalStorage || tokenSessionStorage;
+};
+
+export const removeToken = () => {
+  sessionStorage.removeItem(TOKEN_KEY);
+  localStorage.removeItem(TOKEN_KEY);
+};
