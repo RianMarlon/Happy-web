1df560885f0446d92886ac09d3e7c201aa0db993
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-30 12:39:49 -0300

    Adicionando o AuthProvider no App.tsx do projeto web

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,15 @@ import Routes from './routes';
 import 'leaflet/dist/leaflet.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 
+import { AuthProvider } from './contexts/AuthContext';
+
 import './assets/styles/global.css';
 
 function App() {
   return (
-    <Routes />
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
 
