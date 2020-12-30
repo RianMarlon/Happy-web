40577acd544b27b6527495654505eddf6284fe7a
Rian Marlon <rianmarlon20@yahoo.com>
2020-12-30 12:36:23 -0300

    Adicionando a estrutura do componente SplashScreen do projeto web

---
+++ src/components/SplashScreen/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Logo from '../../components/Logo';
+
+import './styles.css';
+
+function SplashScreen() {
+  return (
+    <div className="splash-screen-container">
+      <Logo />
+    </div>
+  );
+}
+
+export default SplashScreen;
